52049df1bb3718bf509fc5c602f4d2fa26929313
zainiCodes <zainulabdinpk19@gmail.com>
2026-02-25 23:03:42 +0500

    added nextauth in project

---
+++ src/utils/auth.ts
@@ -0,0 +1,20 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { prisma } from "./prisma"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                return null
+            },
+        }),
+
+    ],
+})
